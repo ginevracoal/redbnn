81149dd272db5efcb46e1f824c215c09b7179f49
Ginevra Carbone <ginevracoal@gmail.com>
2021-07-22 12:27:35 +0200

    new tests

--- tests.py
+++ tests.py
@@ -43,7 +43,7 @@ class TestredBNN(unittest.TestCase):
         model.load(filename=filename, savedir='examples/')
         model.evaluate(dataloaders['test'], device=device)
 
-    def test_bayesian_inference_svi(self):
+    def test_bayesian_inference_svi_blocks(self):
         warnings.filterwarnings('ignore')
         from pyro.distributions import Normal
         import torch.nn as nn
@@ -52,6 +52,52 @@ class TestredBNN(unittest.TestCase):
 
         network = redBNN(architecture=architecture, input_size=input_size, num_classes=num_classes, 
                     inference='svi', reduction='blocks', bayesian_idx=4)
+        network.train(dataloaders=dataloaders, num_iters=2, device=device, eval_samples=3, svi_iters=2)
+
+        old_state_dict = network.network.state_dict()
+
+        network.to(device)
+
+        network.network.eval()
+        with torch.no_grad():
+
+            inputs = next(iter(dataloaders['train']))[0]
+            inputs = inputs.to(device)
+
+            preds = []  
+            for seed in [0,1,2]:
+                pyro.set_rng_seed(seed)
+                guide_trace = poutine.trace(network.guide).get_trace(inputs) 
+
+                weights = {}
+                for key, value in network.network.state_dict().items():
+                    weights.update({str(key):value})
+
+                for param_name, param in network.bayesian_weights.items():
+
+                    dist = Normal(loc=guide_trace.nodes[param_name+"_loc"]["value"], 
+                                  scale=softplus(guide_trace.nodes[param_name+"_scale"]["value"]))
+                    w = pyro.sample(param_name, dist)
+                    weights.update({param_name:w})
+
+                basenet_copy = copy.deepcopy(network.network)
+                basenet_copy.load_state_dict(weights)
+                preds.append(basenet_copy.forward(inputs))
+
+                assert torch.all(torch.eq(old_state_dict['conv1.weight'], 
+                                 basenet_copy.state_dict()['conv1.weight']))
+                assert torch.all(~torch.eq(old_state_dict['layer1.0.conv1.weight'], 
+                                 basenet_copy.state_dict()['layer1.0.conv1.weight']))
+
+    def test_bayesian_inference_svi_layers(self):
+        warnings.filterwarnings('ignore')
+        from pyro.distributions import Normal
+        import torch.nn as nn
+        import torch.nn.functional as nnf
+        softplus = torch.nn.Softplus()
+
+        network = redBNN(architecture=architecture, input_size=input_size, num_classes=num_classes, 
+                    inference='svi', reduction='layers', bayesian_idx=4)
         network.train(dataloaders=dataloaders, num_iters=2, device=device, eval_samples=3, svi_iters=2)
 
         old_state_dict = network.network.state_dict()
