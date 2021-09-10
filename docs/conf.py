4067b48a1e07756f5526f225bb40a9058196a699
Ginevra Carbone <ginevracoal@gmail.com>
2021-09-10 16:11:04 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Ginevra Carbone'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','furo']
 napoleon_google_docstring = True
 napoleon_use_param = False
 napoleon_use_ivar = True
@@ -52,4 +52,4 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = "_static/new_logo.png"
+html_logo = "_static/logo1.png"
